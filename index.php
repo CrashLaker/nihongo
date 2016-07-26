9bbc4b5be3780c2a6a4d36a3f34c70bf0d24e88b
CrashLaker <crashlaker@gmail.com>
2016-07-26 17:08:44 +0000

    added mbstring function

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-Hello World
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <style type="text/css">
+        
+        
+    </style>
+</head>
+<body>
+<?php
+
+$vowels = explode(", ", "a, i, u, e, o");
+$consonants = explode(", ", "-, k, s, t, n, h, m, y, w, r, n*");
+
+$hiragana = mbStringToArray("あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもや　ゆ　よわ　　　をらりるれろん　　　　");
+$katakana = mbStringToArray("アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤ　ユ　ヨワ　　　ヲラリルレロン　　　　");
+
+
+?>
+
+
+
+
+</body>
+</html>
