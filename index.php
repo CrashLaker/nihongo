33b0b7e2af37e274001b0ba6ac99fe1b60dba1a1
CrashLaker <crashlaker@gmail.com>
2016-07-26 17:53:36 +0000

    fixed printing positions

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <head>
     <meta charset="UTF-8"/>
     <style type="text/css">
+        <?php
+            $size = 28;
+        ?>
+        *{margin:0; padding:0;}
+        #container{position:absolute; top:15px; left: 10px;}
         table{float:left;border-collapse:collapse;}
-        td{width:25px; height:25px;border:1px solid black !important; vertical-align:middle; text-align:center;}
+        td{width:<?php echo $size; ?>px; height:<?php echo $size; ?>px;border:1px solid black !important; vertical-align:middle; text-align:center;}
         
     </style>
 </head>
